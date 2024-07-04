1d62044aff363f530a0f495e49450d4c660188e6
Mbformation <immvp@hotmail.fr>
2024-07-04 21:20:53 +0200

    fix: :bug: Added modal to parent component

--- src/pages/CreateEmployee.jsx
+++ src/pages/CreateEmployee.jsx
@@ -1,5 +1,7 @@
 import Form from "../components/Form";
+import Modal from "../components/Modal";
 import NavBar from "../components/NavBar";
+
 function CreateEmployee() {
   return (
     <>
@@ -8,6 +10,7 @@ function CreateEmployee() {
       </header>
       <h1>Create Employee</h1>
       <Form />
+      <Modal />
     </>
   );
 }
