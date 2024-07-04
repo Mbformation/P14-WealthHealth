ce84ce51713c9bddcac646257ba7466dab6c65cb
Mbformation <immvp@hotmail.fr>
2024-07-04 21:25:28 +0200

    feat: :sparkles: Added form to the page

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,13 +1,96 @@
+import { states } from "../data/states.js";
+import { departments } from "../data/departments.js";
+import DatePicker from "datepicker-react-lib";
+import { useContext, useState } from "react";
+import { EmployeeContext } from "../utils/EmployeeProvider";
+import { Dropdown } from "primereact/dropdown";
+
 function Form() {
+  const { addEmployee } = useContext(EmployeeContext);
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [dateOfBirth, setDateOfBirth] = useState("");
+  const [startDate, setStartDate] = useState("");
+  const [department, setDepartment] = useState("");
+  const [street, setStreet] = useState("");
+  const [city, setCity] = useState("");
+  const [state, setState] = useState("");
+  const [zipCode, setZipCode] = useState("");
   function handleSubmit(e) {
     e.preventDefault();
-    const username = e.target.username.value;
-    const password = e.target.password.value;
-    const isRemembered = e.target.rememberme.checked;
+    document.querySelector(".modal").style.display = "block";
+    addEmployee({
+      firstName,
+      lastName,
+      startDate,
+      department,
+      dateOfBirth,
+      street,
+      city,
+      state: state.abbreviation,
+      zipCode,
+    });
   }
   return (
     <>
-      <form method="post" onSubmit={handleSubmit}>
+      <form id="create-employee" method="post" onSubmit={handleSubmit}>
+        <label>
+          First Name
+          <input type="text" onChange={(e) => setFirstName(e.target.value)} />
+        </label>
+
+        <label>
+          Last Name
+          <input type="text" onChange={(e) => setLastName(e.target.value)} />
+        </label>
+        <label>
+          Date of Birth
+          <DatePicker onChange={(value) => setDateOfBirth(value)} />
+        </label>
+
+        <label>
+          Start Date
+          <DatePicker onChange={(value) => setStartDate(value)} />
+        </label>
+
+        <fieldset className="address">
+          <legend>Address</legend>
+
+          <label>
+            Street
+            <input type="text" onChange={(e) => setStreet(e.target.value)} />
+          </label>
+
+          <label>
+            City
+            <input type="text" onChange={(e) => setCity(e.target.value)} />
+          </label>
+          <label>
+            State
+            <Dropdown
+              value={state}
+              options={states}
+              onChange={(e) => setState(e.value)}
+              optionLabel="name"
+              placeholder="Select a State"
+            />
+          </label>
+          <label>
+            State
+            <Dropdown
+              value={department}
+              options={departments}
+              onChange={(e) => setDepartment(e.value)}
+              optionLabel="value"
+              placeholder="Select a Department"
+            />
+          </label>
+          <label>
+            Zip Code
+            <input type="number" onChange={(e) => setZipCode(e.target.value)} />
+          </label>
+        </fieldset>
+
         <button className="submit-btn" type="submit">
           Save
         </button>
